7c12031d54abf0c6ae59bbfc56aaea5b96907708
azfarus <samazfar52@gmail.com>
2026-02-26 10:46:38 +0600

    lab2-updated

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-
-#pass the folder as the first argument to the script
+# this compilation script is tested on a texlive-full installation in fedora43
+# pass the folder as the first argument to the script
 
 latexmk -pdf $1/*.tex -cd && latexmk -pdf $1/*.tex -cd -c
+rm -r $1/*.aux $1/*.fdb_latexmk $1/*.fls $1/*.log $1/*.out $1/*.toc
